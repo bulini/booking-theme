0ac13fc1a70eb183e1b47672d249374c02800edd
Giuseppe Surace <pinobulini@gmail.com>
2014-05-28 17:59:57 +0200

    footer test

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
       <!-- FOOTER -->
       <footer>
         <p class="pull-right"><a class="btn btn-sm btn-default" href="#">Back to top</a></p>
-        <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p>&copy; 2014 <?php bloginfo('siteurl'); ?><a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
 
     </div><!-- /.container -->
